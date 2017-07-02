0efa7c9465dfdc315a07508af01ba14dfe1d16ee
Artem Rosnovskiy <inbox@podtema.com>
2017-07-02 13:10:53 -0700

    minor tweaks

--- dice.js
+++ dice.js
@@ -1,14 +1,14 @@
 function Dice(sides) {
 	this.sides = sides;
-	}
+	};
 
 Dice.prototype.roll = function() {
 	let randomNumber = Math.floor(Math.random() * this.sides) + 1;
 	return randomNumber;
-};
+	};
 
 button.onclick = function() {
-	let placeholder = document.getElementById("placeholder");
+	let placeholder = document.querySelector("#placeholder");
 	let dice = new Dice(6);
 	placeholder.innerHTML = dice.roll();
 	};
